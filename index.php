e73ef1e63a7f5df44c3fcc4b09144237990eda78
Julien RIPOUTEAU <jripoute@student.42.fr>
2015-04-18 09:58:11 +0200

    index

--- index.php
+++ index.php
@@ -3,18 +3,25 @@ session_start();
 
 /* Fichier a include */
 require ('app/Autoloader.class.php');
-	App\Autoloader::register();
-	$config = App\Config::getInstance();
-
-	$bdd = App\database\Database::getInstance_bdd($config->get("db_name"), $config->get("db_user"), $config->get("db_pass"), $config->get("db_host"));
-	require ('./functions/functions.php');
-
+App\Autoloader::register();
+$config = App\Config::getInstance();
+$bdd = App\database\Database::getInstance_bdd($config->get("db_name"), $config->get("db_user"),
+	$config->get("db_pass"), $config->get("db_host"));
+require ('./functions/functions.php');
+require ('app/trait/Doc.trait.php');
+require ('app/Map.class.php');
+require ('app/IShip.class.php');
+require ('app/Ship.class.php');
+require ('app/HonorableDuty.class.php');
+require ('app/SwordOfAbsolution.class.php');
 ?>
+
 <html>
 <head>
 	<meta charset='UTF-8'>
 	<title>..:: Warhammer 42K ::..</title>
 	<link rel="stylesheet" type="text/css" href="./styles/general.css">
+	<link rel="stylesheet" type="text/css" href="./styles/map.css">
 </head>
 <body>
 	<div id="global">
@@ -26,7 +33,9 @@ require ('app/Autoloader.class.php');
 		</div>
 		<div id="general">
 			<div id="content">
-				<p class="ecrit"><!-- Contenu --></p>
+				<p class="ecrit">
+				<?PHP require ("functions/create_table_map.php")?>
+				</p>
 			</div>
 			<div id="menu">
 				<p class="ecrit"><!-- Menu --></p>
@@ -37,5 +46,8 @@ require ('app/Autoloader.class.php');
 			</div>
 		</div>
 	</div>
+<?php
+?>
+
 </body>
 </html>
