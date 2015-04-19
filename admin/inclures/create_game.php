561a99fca932db3118007a86fbdf6aecc3d7e371
Julien RIPOUTEAU <jripoute@student.42.fr>
2015-04-19 22:22:19 +0200

    protection

--- admin/inclures/create_game.php
+++ admin/inclures/create_game.php
@@ -6,18 +6,23 @@
 <label for="Player1">Player 1 :</label><br>
 <?php
 	$pl = select_all_user($bdd);
-	sort($pl);
-	echo "<select name=\"player1\" id=\"Player1\" required>";	
-	foreach ($pl as $k => $v) {
-		if (!check_game($bdd, $pl[$k]['id_player']))
-			echo "<option value=\"".$pl[$k]['id_player']."\">".$pl[$k]['login']."</option>"; 
+		echo "<select name=\"player1\" id=\"Player1\" required>";
+	if (!empty($pl))
+	{
+		foreach ($pl as $k => $v) {
+			if (!check_game($bdd, $pl[$k]['id_player']))
+				echo "<option value=\"".$pl[$k]['id_player']."\">".$pl[$k]['login']."</option>";
+		}
 	}
 	echo "</select>";
 	echo "<br /><br /><label for=\"Player2\">Player 2 : </label><br>";
-	echo "<select name=\"player2\" id=\"Player2\" required>";	
-	foreach ($pl as $x => $y) {
-		if (!check_game($bdd, $pl[$x]['id_player']))
-			echo "<option value=\"".$pl[$x]['id_player']."\">".$pl[$x]['login']."</option>"; 
+	echo "<select name=\"player2\" id=\"Player2\" required>";
+	if (!empty($pl))
+	{
+		foreach ($pl as $x => $y) {
+			if (!check_game($bdd, $pl[$x]['id_player']))
+				echo "<option value=\"".$pl[$x]['id_player']."\">".$pl[$x]['login']."</option>";
+		}
 	}
 	echo "</select>";
 ?>
